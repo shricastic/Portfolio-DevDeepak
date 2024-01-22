c59b4cc3bd3df5484d732da5758e19a4e44003cf
Deepak <pokemongamingworld123@gmail.com>
2024-01-22 14:24:35 +0545

    fix: fixed a typo in Jasper's name

--- components/my-components/Contact/Testimonials.tsx
+++ components/my-components/Contact/Testimonials.tsx
@@ -10,11 +10,11 @@ const TestimonialList = [
       "What a pleasure to work with Deepak.. Young dev who's clearly talented with a strong passion for your complete satisfaction and solving any issues that should arise. I'm looking forward to working with Deepak further and appreciate all your efforts. Thank you!",
   },
   {
-    giver: "Jasper Jougen",
+    giver: "Jasper Jongen",
     status: "Upwork Client",
     feedback:
       "I requested Deepak to tweak a project and he did a great job at it. Perfectly listened to the provided feedback, gave suggestions where to improve and fast delivery. Very pleasant to work with him. Eager to learn knew things and knowledgeable dev",
-    image: "/jasper_jougen.jpg",
+    image: "/jasper_jongen.jpg",
   },
 ];
 export default function Testimonials({}: Props): ReactElement {
