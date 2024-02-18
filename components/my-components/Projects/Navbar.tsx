bb7e0f79b6a181ee66feb2b3848658fae922aed1
Deepak <pokemongamingworld123@gmail.com>
2024-02-18 22:17:22 +0545

    refact: Changed logo in the description pages

--- components/my-components/Projects/Navbar.tsx
+++ components/my-components/Projects/Navbar.tsx
@@ -1,24 +1,22 @@
 "use client";
 import React, { ReactElement } from "react";
 import { ChevronLeft } from "lucide-react";
-
+import Link from "next/link";
 export default function Navbar(): ReactElement {
   return (
     <div className="sticky top-0 flex items-center justify-between text-white h-16 mb-4 backdrop-filter backdrop-blur-lg">
-      <a
-        onClick={() => {
-          window.history.back();
-        }}
-        className="decoration-none flex items-center justify-center rounded-md border border-white px-2 text-white no-underline hover:scale-95 transition-all duration-150"
+      <Link
+        href="/"
+        className="decoration-none h-8 w-max flex items-center justify-center rounded-md border border-white px-2 text-white no-underline hover:scale-95 transition-all duration-150"
       >
         <ChevronLeft />
-        Back
-      </a>
+        Home
+      </Link>
 
       <img
         src={"/logo.png"}
         alt={"Logo"}
-        className="h-16 w-16 object-cover object-bottom rounded-full"
+        className="h-10 w-10 object-fit object-bottom rounded-full"
       />
     </div>
   );
